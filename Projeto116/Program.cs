5e897bca5ee20614636f0313c9586acfc28e7b64
Junior Fernando <junior_618@hotmail.com>
2023-05-12 16:00:39 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto116/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0.0, 500.);
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0; // error
+
+            Account acc1 = bacc;        }
+    }
+}
